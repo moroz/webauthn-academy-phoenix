9daf3759da18b456ca2ddbf77365a70e6112e1a7
Karol Moroz <karol@moroz.dev>
2024-05-10 22:23:00 +0800

    Initial commit

---
+++ lib/academy_web/router.ex
@@ -0,0 +1,37 @@
+defmodule AcademyWeb.Router do
+  use AcademyWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {AcademyWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", AcademyWeb do
+    pipe_through :browser
+
+    get "/", PageController, :home
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", AcademyWeb do
+  #   pipe_through :api
+  # end
+
+  # Enable Swoosh mailbox preview in development
+  if Application.compile_env(:academy, :dev_routes) do
+
+    scope "/dev" do
+      pipe_through :browser
+
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
